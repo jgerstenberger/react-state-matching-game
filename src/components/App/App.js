d29a4025d7b4a6ff24521a756e56527b25f04c07
Jason Gerstenberger <jason@gerstenberger.name>
2020-10-09 15:48:16 -0400

    .

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils'
+import { createTiles, indexOfSelected } from '../../misc/utils'
 
 import './App.css';
 
@@ -27,8 +27,41 @@ class App extends Component{
 
   startGame(numTiles) {
     this.setState((state) => ({
-      playing: true, previousTileIndex: null, toBeCleared: null, tiles: createTiles(this.state.numTiles)
+      playing: true, previousTileIndex: null, toBeCleared: null, tiles: createTiles(this.state.numTiles, this.handleTileClicked)
     }));
+  }
+
+  handleTileClicked(id, color) {
+    this.setState((state) => {
+      const tiles = this.state.tiles
+      let toBeCleared = this.state.toBeCleared
+      const selectedTileIndex = indexOfSelected(tiles, id, color)
+      let previousTileIndex = this.state.previousTileIndex
+
+      if (toBeCleared != null) {
+        tiles[toBeCleared[0]].selected = false
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
+      }
+
+      tiles[selectedTileIndex].selected = true
+
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
+        if (previousTile.id !== selectedTile.id && previousTile.color !== selectedTile.color) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+          previousTileIndex = null
+        }
+      } else {
+        previousTileIndex = selectedTileIndex
+      }
+      return {toBeCleared, tiles, previousTileIndex}
+    });
   }
 }
 
